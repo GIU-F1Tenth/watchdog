abd7d4ef4143e910fba6c6c6a9c9747fc1a671ec
Yousuf Aboelahab <yousuf.moh.dahab@outlook.com>
2025-11-16 13:50:38 +0100

    chore: address PR unresolved threads
    - Stop tracking install/ (already in .gitignore) to ensure build artifacts never enter the repo
    - Remove deprecated FSM integration module (watchdog/fsm_integration.py)
    - Remove duplicate F1TENTH launch files; keep single canonical launch/watchdog.launch.py
    
    This resolves:
    - comment on FSM integration existence
    - comment about having 2 different launch files
    - comment about install/ files appearing in PR

--- install/watchdog/share/watchdog/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(watchdog_IDL_FILES "msg/SanityWarning.idl;msg/SensorHealth.idl;msg/SanitySummary.idl")
-set(watchdog_INTERFACE_FILES "msg/SanityWarning.msg;msg/SensorHealth.msg;msg/SanitySummary.msg")
